017dac6f6c72b0f52b2d9278320c614f51ef68ef
Aharon Seidman <aharondov1@gmail.com>
2023-03-14 23:56:51 -0400

    edited (for clarity and further explanation) the package-info level javadoc for the ui package

--- src/edu/touro/mco152/bm/ui/package-info.java
+++ src/edu/touro/mco152/bm/ui/package-info.java
@@ -2,6 +2,6 @@
  * This package contains classes for the user interface.
  * Each class creates specific parts of the UI, and
  * each contain various methods that allow for
- * adjusting those aspects.
+ * adjustment of the parts of the UI they create.
  */
 package edu.touro.mco152.bm.ui;
