05e0c27c825f0922a3290594645e39d7173a7c8c
ChanochW <cwinogra@student.touro.edu>
2023-03-15 13:18:11 -0400

    Improved the package level JavaDoc for the UI package.

--- src/edu/touro/mco152/bm/ui/package-info.java
+++ src/edu/touro/mco152/bm/ui/package-info.java
@@ -3,5 +3,8 @@
  * Each class creates specific parts of the UI, and
  * each contain various methods that allow for
  * adjusting those aspects.
+ * <p>
+ * This package is dependent on java swing library.
+ * The {@link edu.touro.mco152.bm.ui.Gui} class manages the other components within this package.
  */
 package edu.touro.mco152.bm.ui;
